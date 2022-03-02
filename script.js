9568c115dfbfc986e9c12d1eb95731e4370d3f08
apokharel123 <Student@Bluegrasss-MacBook-Pro-2.local>
2022-03-02 01:23:21 -0500

    JavaScript Variables Exercise

---
+++ script.js
@@ -0,0 +1,71 @@
+//1
+const age=20;
+console.log(age);
+
+//2
+ num =20;
+console.log(num);
+
+//3
+num=15;
+ console.log(15);
+
+ //or
+ // var num=20;
+  //num=15;
+  //console.log(num);
+
+
+//4
+num=num+7;
+console.log(num);
+
+//OR
+//var num=15;
+//num=num+7;
+//console.log(num);
+
+//5
+num--;
+console.log(num);
+
+//OR
+//var num=22;
+//num=num-1;
+//console.log(num);
+
+
+
+//6
+//num="This is not a number";
+//console.log(num);
+
+//7
+let blank=""; 
+
+//8
+
+console.log(blank);
+//or
+//let blank;
+//console.log(blank);
+
+//9
+node script.jsblank=null;
+
+//BONUS
+//1
+const quarter=0.25, dime=0.10, nickel=0.05, penny=0.01;
+
+//2
+console.log(quarter,dime, nickel,penny);
+
+//3
+let letters="qwertyuiop"
+
+//4
+console.log(letters[9]);
+
+
+
+
